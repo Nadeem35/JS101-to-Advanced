9a33fb334a8dd3d2543f18e06ab4146dc7c32ba2
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-11-23 23:40:56 +0530

    Add files via upload
    Add Javascript code.

---
+++ Tutorials/00 Basics.js
@@ -0,0 +1,16 @@
+console.time('Your code took time');
+console.log("This is console");
+console.log(20+5);
+console.log(43-2);
+console.log(43*2);
+console.log(43/2);
+console.log(43%2);
+console.log(true);
+console.log([2,4,6,7,4,9]);
+console.log({nadeem:'This', marks:92});
+console.table({nadeem:'This', marks:92});
+console.warn("This is large warning");
+// console.clear();
+console.timeEnd('Your code took time');
+console.assert(160<150, 'Age>150 is not possible');
+console.error("This is an errors");
