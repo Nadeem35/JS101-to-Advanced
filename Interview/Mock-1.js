7274d5a6eb52f5762ba2af557b03a9562f3b1388
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-12-06 02:10:26 +0530

    Add files via upload
    Add Mock interview Questions

---
+++ Interview/Mock-1.js
@@ -0,0 +1,81 @@
+/*
+
+1.What is HTML?
+2. Semantic Tags in HTML
+3. four exmples of semantic tags
+4. apart from meaning to browser does semantic elements help other way?
+5. what are tags & attributes?
+6. difference between div and span?
+7. examples of inline elements.
+8. apart from width difference is there sny other differnce?
+9. can we give height & width in inline elements?
+10. diff between canvas and nsvg
+11. full form of css
+12. tell meaning of cascading in css
+13. if we give some styling to parent will it apply on the child also
+14. what is specificity?
+15. default value of position property
+16. difference between position relative and absolute
+17. what is flexbox?
+18. if we have a container and inside container there are two elements span 1 and span2 & we give display:flex to parent will it apply to its children as well?
+19. what is box model in css?
+20. default value of box-sizing
+21. diff in content box border box?
+22. why do we need javascript ?
+23. what are different data types in javascript?
+24. why do we have categories like premetive and non premitive in JS?
+25. in how many ways we csn create variabls in JS?
+26. diff b/w let and var and there scope?
+27. diff b/w parameter and argument?
+28. difference b/w slice and splice methods?
+29. why do we use map method in array?
+30. what are promises in javascript?
+31. what is DOM?
+32. EVENT BUBBLING & CAPTURING
+33. why do we need React?
+34. diff b/w imperative and declarative?
+35. what is concept of state in React?
+36. why dont we use normal variable why state?
+37. GUESS OUTPUT::
+ var x=20 
+function foo(){
+console.log(x)
+var x=10
+}
+foo()
+// if change x=10 to let x=10 what will happen?
+38. console.log('Start')
+setTimeout(()=>{
+console.log('timeout')
+},0)
+console.log('End')
+//the order in which it will run?
+39. setTimeout(()=>{
+console.log('timeout')
+},0)
+Promise.resolve().then(()=>console.log('Promise'))
+console.log('End')
+40. async funtion foo(){
+return 'Hello World'
+}
+const result = foo()
+console.log(result)
+41. [1,2]==[1,2]
+42. const user1={
+name: 'john',
+age:25,
+address:{
+city:'Mumbai',
+state:'Mahrashtra'
+},}
+const user2 = user1
+user2.name = 'Ramesh'
+user2.address.city='Pune'
+console.log(user1)
+console.log(user2)
+43. const numbers= [0,1,2,3,4,5,6] //filter out all values less than 3
+44. create a string variable add a string in lowerCase and convert first letter of each to uppercase OR can you transform it into array.
+45. PROJECT:: create a TODO list in which there will be input and add button delete also
+
+
+*/
