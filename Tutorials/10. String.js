1a916d0bb0a8047c7b6a5c1c8c9e174e251628f2
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-12-20 21:04:18 +0530

    Update 10. String.js

--- Tutorials/10. String.js
+++ Tutorials/10. String.js
@@ -4,12 +4,12 @@ const name = `nadeem`
 const name1 = 'naheem'
 console.log(`${name} and ${name1} both are brothers each other`)
 
-//Accessing String Charecters
+//----- Accessing String Charecters
 const a = 'HelloN';
 console.log(a[0]);
 console.log(a.charAt(5));
 
-// JS is Case-Sensitive
+// ------ JS is Case-Sensitive
 const x = 'Hello';
 const y = new String('Hellow')
 console.log(x)
@@ -22,28 +22,29 @@ console.log(typeof y)  // Object
 const text1 = 'hello';
 const text2 = 'world';
 const text3 = '        JavaScript       ';
-// concatenating two string
+
+// ------ concatenating two string
 const result1 = text1.concat(text2);
 console.log(result1);
 
-// converting lower case into upper case
+// ------ converting lower case into upper case ------
 const result2 = text2.toLocaleUpperCase()
 console.log(result2)
 
-// converting upper to lower
+// ------ converting upper to lower ------
 const result3 = text3.toLocaleLowerCase();
 console.log(result3)
 
-// converting string to array
+// ---- converting string to array ------
 const result4 = text1.split()
 console.log(result4);
 
-// slicing the string
+// ------ slicing the string ------
 const result5 = text2.slice(2,5)
 console.log(result5)
 
 
-// --- String() function
+// ------ String() function ------
 const p = 342;
 const q = true;
 // converting to string
@@ -52,7 +53,7 @@ const q1 = String(q)
 console.log(p1)
 console.log(q1)
 
-// Escape charecter
+// ------ Escape charecter ------
 const name0 = 'This is \'Salba\'.';
 console.log(name0)
 
