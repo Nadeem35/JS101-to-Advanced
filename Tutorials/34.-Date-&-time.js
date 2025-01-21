b937249fac00d01bec5adf70df31cf97d7c17249
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-01-21 22:40:58 +0530

    Add files via upload

---
+++ Tutorials/34.-Date-&-time.js
@@ -0,0 +1,27 @@
+
+// -> new Date()
+// -> new Date(milliseconds)
+// -> new Date(Date string)
+// -> new Date(year, month, day, hours, minutes, seconds, milliseconds)
+
+
+//  >>>>>>>>>--- new Date() ---<<<<<<<<<<<<<
+
+// const samaye = new Date()
+// console.log(samaye)
+
+
+//  >>>>>>>>>--- new Date(millisecond) ---<<<<<<<<<<<<<
+// const time1 = new Date(0)
+// console.log(time1)
+
+// const time2 = new Date(31536000000)
+// console.log(time2)
+
+// const time3 = new Date(2678400000)
+// console.log(time3)
+
+
+//  >>>>>>>>>--- new Date(Date string) ---<<<<<<<<<<<<<
+const time4 = new Date('1998, feb, 2')
+console.log(time4)
