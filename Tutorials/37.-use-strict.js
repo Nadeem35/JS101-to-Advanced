bb22fe6deca9f6e4cfcf03666b964c902aab75e6
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-01-23 23:41:57 +0530

    Add files via upload
    use strict mode in Javascript

---
+++ Tutorials/37.-use-strict.js
@@ -0,0 +1,21 @@
+
+console.log('nadeemusshan')
+
+sbab = 5;
+
+'use strict';
+neam = 45;
+
+
+console.log(neam)
+
+
+
+function wada() {
+
+    // 'use strict';
+
+    sun = 'INSHA ALLAH';
+}
+wada();
+
