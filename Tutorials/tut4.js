29c4acde1561c7137a4bcc551e6b33bbe4099973
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-01-24 23:56:59 +0530

    Add files via upload

---
+++ Tutorials/tut4.js
@@ -0,0 +1,43 @@
+//Primitive data types
+
+//string
+var name = "nadeem CSE";
+var name = "Salba"
+console.log("My Data type is string " + name);
+console.log("Data type is " + (typeof name) );
+
+// Numbers
+let marks = 37;
+console.log("Data type is " + (typeof marks))
+
+// Boolean
+let isDrive = true;
+console.log("Data type is " + (typeof isDrive));
+
+// Null
+let nullvar = null;
+console.log("Data type is " + (typeof nullvar))
+
+// undefined
+let undef = undefined;
+console.log("Data type is " + (typeof undef))
+
+// Reference data types
+let thisarr = [3,4,5,63,8, false, "nadeem"]
+console.log("Data type is " + (typeof thisarr))
+
+// objects Literals
+let mrks = {
+    nadeem: 42,
+    salba: 43,
+    uzair: 32,
+}
+console.log(typeof mrks)
+
+function fname() {
+    
+}
+console.log(typeof fname)
+
+let date = new Date();
+console.log(typeof date);
