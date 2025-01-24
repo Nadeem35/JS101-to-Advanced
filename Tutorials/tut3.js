d87fb4961ff5a141bb27adf6d49093ed8a270fb5
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-01-24 23:48:08 +0530

    Add files via upload

---
+++ Tutorials/tut3.js
@@ -0,0 +1,32 @@
+console.log('tut3');
+// Variables in JS
+// Variables in var, let & const
+var name = "N'ad`eem";
+var channel1 = "Millat Times"
+var marks = 92;
+console.log(name, channel1, marks);
+     // Rules for creating JavaScripts variables
+/* 1. Can't start with number
+   2. Can start with letter, letter+numbers, _ or $
+ */
+var _city = 'hydrabad'
+var $city = 'Sikandrabad'
+console.log(_city)
+console.log($city)
+
+const ownerName = "Suleman";
+// const ownerName = "Nadeem";
+console.log(ownerName)
+const fruit = 'date'
+console.log(fruit)
+
+{
+    let city = "Bareilly"
+    city = 'Badayun'
+    console.log(city)
+}
+const arr1 = [3,6,7,5,67,45,2]
+arr1.push(34)
+console.log(arr1)
+
+/*  */
