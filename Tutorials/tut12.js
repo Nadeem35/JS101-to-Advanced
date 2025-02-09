a4b2930bf6aad014722ed40d66837dd2b9761865
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-02-09 22:41:45 +0530

    Add files via upload

---
+++ Tutorials/tut12.js
@@ -0,0 +1,16 @@
+console.log("This is 12th Lecture");
+
+let a = document;
+a = document.all;
+a = document.body;
+a = document.forms[0];
+
+// Array.form(a).forEach(function (element) {
+ // console.log(element);
+// })
+
+// a = document.links[0];
+a = document.links[0].href;
+console.log(a);
+
+// Now use => document.image and document.script and print the list of images and scripts on an html page
