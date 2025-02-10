6103fc2abb7257f7f3ef71e9ddc1bc9362bd0328
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-02-10 23:34:29 +0530

    Add files via upload
    New topic added Javascript

---
+++ Tutorials/tut13.js
@@ -0,0 +1,15 @@
+console.log("This is 13th Lecture");
+// You have to create a variable which is a string conataining the text which is a link you are interested in.
+
+// You have fetch all the links from a given page which conatains this text
+
+
+let a = 34;
+a = document;
+a = document.script;
+arr = [5,6,7,3,9,7]
+
+// a = document.images;
+// a = document.links[0];
+// console.log(arr)
+console.log(a)
