4edab3578be243de79763061068d310f69ba920e
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-02-17 23:54:27 +0530

    Add files via upload
    Learn Typecript

---
+++ TS/Basic/variable.js
@@ -0,0 +1,27 @@
+"use strict";
+console.log("welcome to typescript lecture by 'Sherians Coding School'.");
+// number
+let age = 12;
+console.log(age);
+//-------------------
+// // boolena
+// let isLoggedIn: boolean = true
+// console.log(isLoggedIn)
+// let name1: string  
+// name1 = "Nadeem";
+// name1 = "Uzair"
+// console.log(name1);
+//----------------------
+// let useId: number = 465121323.465
+// useId.toFixed()
+// useId = 11515232;
+// console.log(useId);
+// console.log("Jamia is best uniVersity")
+// -----------------------------------------
+// any (when we not defined data type)
+let hero;
+function getHero() {
+    return "Adira";
+}
+hero = getHero();
+console.log(hero);
