2548da9ce5b9b999deff25ec1a419cc704fd9c95
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-02-20 02:29:18 +0530

    Update tut11.js

--- tut11.js
+++ tut11.js
@@ -1,17 +1,1 @@
-console.log("This is 11th lecture");
 
-let a = window;
-// alert("Hellow nadeem");
-// a = prompt('This will be destroy your PC. Enter your name');
-// a = confirm("Are want to delete your page")
-
-a = window.document;
-// a = window.innerWidth;
-// a = innerWidth;
-// a = window.innerHeight;
-// a = outerHeight;
-// a = outerWidth;
-a = screenX;
-a = location.toString();
-a = window.history;
-console.log(a);
