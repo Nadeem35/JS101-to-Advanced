8cf10223818dfcfe2e906a4a22b29ff4fb88a520
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-02-20 23:12:55 +0530

    Add files via upload

---
+++ TS/String/script.js
@@ -0,0 +1,22 @@
+"use strict";
+// console.log("Nadeem -- N'adira ");
+let value = {
+    name: "Bareilly",
+    population: 458999454,
+    cities: 75
+};
+console.log(value.name);
+let a = 54;
+let b = 12;
+let c;
+c = a + b;
+console.log("412 c");
+console.log("I am TS !");
+let allData = {
+    name: "Nafis",
+    age: 30,
+    adult: true // ✅ Must include this!
+};
+console.log(allData.name);
+console.log(allData.age);
+console.log(allData.adult); // ✅ Works
